0cd036ce1438fdea8a2b71dc1a50eab5f9da4f9c
Utku Enes Alagöz <utkuenesalagoz@icloud.com>
2022-09-26 10:26:23 +0300

    Initial Commit

---
+++ QR/QRApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRApp.swift
+//  QR
+//
+//  Created by utku enes alagöz on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct QRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
