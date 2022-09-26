1e33cf9f649d871e53e95c59b25c7a7ca2317884
Utku Enes Alagöz <utkuenesalagoz@icloud.com>
2022-09-26 11:36:20 +0300

    QR scan

--- QR/ContentView.swift
+++ QR/ContentView.swift
@@ -1,17 +1,42 @@
 //
 //  ContentView.swift
-//  QR
+//  QrScan
 //
-//  Created by utku enes alagöz on 26.09.2022.
+//  Created by utku enes alagöz on 24.09.2022.
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "QR code tarama uygulaması"
+    var scannerSheet: some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+                
+            }
+        )
     }
+    
+    var body: some View {
+        
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            Button("QR kod tara.") {
+                self.isPresentingScanner = true
+            }
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannerSheet
+            }
+            
+        }
+          }
 }
 
 struct ContentView_Previews: PreviewProvider {
